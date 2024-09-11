b7cbd2d99d84870d288577f0dc4cf16931323367
fyx-ssj <79633982+fyx-ssj@users.noreply.github.com>
2024-09-11 15:58:14 +0800

    Update locale-selector.vue

--- src/modules/i18n/components/locale-selector.vue
+++ src/modules/i18n/components/locale-selector.vue
@@ -3,22 +3,12 @@ const { availableLocales, locale } = useI18n();
 
 const localesLong: Record<string, string> = {
   zh: '中文',
-  //en: 'English',
-  //de: 'Deutsch',
-  //es: 'Español',
-  //fr: 'Français',
-  //pt: 'Português',
-  //ru: 'Русский',
-  //uk: 'Українська',
-  //vi: 'Tiếng Việt',
 };
 
 const localeOptions = computed(() =>
   availableLocales.map(locale => ({
-    label: localesLong['zh'],
-    value: 'zh',
-    //label: localesLong[locale] ?? locale,
-    //value: locale,
+    label: localesLong[locale] ?? locale,
+    value: locale,
   })),
 );
 </script>
@@ -27,7 +17,7 @@ const localeOptions = computed(() =>
   <c-select
     v-model:value="locale"
     :options="localeOptions"
-    placeholder="Select a language"
+    placeholder="中文"
     w-100px
   />
 </template>
